49516785a27278446d31fcc60a40d4da036834a8
edward li <edwarli24@gmail.com>
2013-10-18 21:57:32 -0700

    The original commit for the password project

---
+++ Assignment_1.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : Assignment_1.cpp
+// Author      : Edward Li
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "!!!Hello World!!!\n" << endl;
+   cout << "My family name is Li" << endl;
+   cout << "My student ID is 20145885" << endl << endl;
+   cout << "I noticed in the syllabus that we are required to post in the"
+      << " forum every week and each post will be given 1 point for a max of"
+      << " 10 points." << endl << endl;
+	return 0;
+}
+
+/* ---------------------- RUN -------------------------------------
+
+!!!Hello World!!!
+
+My family name is Li
+My student ID is 20145885
+
+I noticed in the syllabus that we are required to post in the forum every
+week and each post will be given 1 point for a max of 10 points.
+
+---------------------------------------------------------------------- */
